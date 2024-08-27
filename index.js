169ced69f9dfd9d8caccf070ccb2b852d00b1195
Roberto Marchetti <marchetti@beefree.io>
2024-08-27 22:23:22 +0200

    feat: directly use Chart.js, remove chartjs-node-canvas

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
-const { CanvasRenderService } = require('chartjs-node-canvas');
-const { fs } = require('fs');
+const { Chart } = require("chart.js/auto");
+const { createCanvas } = require("canvas");
 const Jimp = require('jimp');
 const configuration = require('./sample_data/doughnut');
-const targetImageName = "testimage";
+const SAVE_FILE_NAME = "testimage";
 
-const width = 400;
-const height = 400;
+const CANVAS_WIDTH = 400;
+const CANVAS_HEIGHT = 400;
+const CANVAS_BACKGROUND_COLOR = "#ffffff";
 
-const chartCallback = (ChartJS) => {
-    ChartJS.defaults.global.responsive = true;
-    ChartJS.defaults.global.maintainAspectRatio = false;
-};
+const writeImage = (image) => image.write(`${SAVE_FILE_NAME}.${image.getExtension()}`);
 
 (async () => {
-  const canvasRenderService = new CanvasRenderService(width, height, chartCallback);
-  const imagebuf = await canvasRenderService.renderToBuffer(configuration);
-  await Jimp.read(await imagebuf).then(function (image) {
-    var file = targetImageName + "." + image.getExtension();
-    image.write(file)
-  }).catch(function (err) {
-    console.error(err);
-  });
+  Chart.register(
+    require("chartjs-plugin-datalabels"),
+    {
+      id: "BackgroundColor",
+      beforeDraw: (chart) => {
+        const { ctx } = chart;
+        ctx.save();
+        ctx.fillStyle = CANVAS_BACKGROUND_COLOR;
+        ctx.fillRect(0, 0, chart.canvas.width, chart.canvas.height);
+        ctx.restore();
+      }
+    }
+  );
+  const canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  const ctx = canvas.getContext("2d");
+  const chart = new Chart(ctx, configuration);
+
+  canvas.toBuffer(async (error, buffer) => {
+    chart.destroy();
+    if (error) {
+      console.error(error);
+    }
+    await Jimp.read(await buffer).then(writeImage).catch(function (err) {
+      console.error(err);
+    });
+  }, "image/png");
 })();
